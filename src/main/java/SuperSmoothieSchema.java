3ce3c15be346650700ea787adf590dfa1657208b
Daria Supińska <dariasupinska@gmail.com>
2020-09-15 22:12:28 +0200

    initial commit

---
+++ src/main/java/SuperSmoothieSchema.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class SuperSmoothieSchema {
+    private Map<String, Integer> pointsPerCategory = new HashMap<>();
+
+    public SuperSmoothieSchema(DrinkCatalog catalog) {
+        this.catalog = catalog;
+    }
+
+    private DrinkCatalog catalog;
+
+
+
+    public void setPointsPerCategory(String category, Integer points) {
+        pointsPerCategory.put(category, points);
+    }
+
+    public int getPointsFor(String drink) {
+        return pointsPerCategory.get(categoryOf(drink));
+    }
+
+    private String categoryOf(String drink) {
+        return catalog.getCategoryOf(drink);
+    }
+}
